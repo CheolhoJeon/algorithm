b1e1f68c5ac246d7cf532ce26af3ce30f0548c9f
전철호 <cheolho@wisoft.io>
2018-09-12 12:02:02 +0900

    Q001 수정

--- src/Q001/main.java
+++ src/Q001/main.java
@@ -3,6 +3,7 @@ package Q0001;
 import Q001.RecursionSumZeroToInput;
 import Q001.RepetitionSumZeroToInput;
 import Q001.SumZeroToInput;
+import Q001.Timer;
 
 import java.util.Scanner;
 
@@ -11,20 +12,18 @@ public class main {
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
-    SumZeroToInput sumZeroToInput;
-    long calculateStartTime;
 
     System.out.printf("입력: ");
     int input = scanner.nextInt();
 
-    sumZeroToInput = new RecursionSumZeroToInput();
-    calculateStartTime = System.nanoTime();
-    System.out.println("결과(Recursion): " + sumZeroToInput.calculate(input));
-    System.out.println("실행시간: " + (System.nanoTime() - calculateStartTime) / 1000000 + " ms");
+    execute(input, new RecursionSumZeroToInput());
+    execute(input, new RepetitionSumZeroToInput());
+  }
 
-    sumZeroToInput = new RepetitionSumZeroToInput();
-    calculateStartTime = System.currentTimeMillis();
-    System.out.println("결과(Repetition): " + sumZeroToInput.calculate(input));
-    System.out.println("실행시간: " + (System.nanoTime() - calculateStartTime) / 1000000 + " ms");
+  private static void execute(int input, SumZeroToInput sumZeroToInput) {
+
+    Timer.start();
+    System.out.println("결과(" + sumZeroToInput.getMethod() + "): " + sumZeroToInput.calculate(input));
+    System.out.println("실행시간: " + Timer.stop() + " ns");
   }
 }

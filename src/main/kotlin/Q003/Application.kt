ac7ab69284b6a13492d3acb3c1586fcf1d2e8848
전철호 <cheolho@wisoft.io>
2021-04-06 17:49:24 +0900

    update Q0003

---
+++ src/main/kotlin/Q003/Application.kt
@@ -0,0 +1,17 @@
+package Q003
+
+import atomictest.eq
+
+fun main() {
+  solution("abcde") eq "c"
+  solution("qwer") eq "we"
+}
+
+// fun solution(s: String) = s.substring((s.length - 1) / 2, s.length / 2 + 1)
+// fun solution(s: String) = s.slice(((s.length - 1) / 2)..(s.length / 2))
+
+fun solution(s: String): String {
+  val start = (s.length - 1) / 2
+  val end = s.length / 2
+  return s.slice(start..end)
+}
--- src/main/kotlin/Q003/MiddleLetterSelector.java
+++
@@ -1,9 +0,0 @@
-package Q003;
-
-public class MiddleLetterSelector {
-
-  static String select(String str) {
-    return str.substring((str.length() - 1) / 2, str.length() / 2 + 1);
-  }
-
-}

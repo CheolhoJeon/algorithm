9c32d75520b20316690eec7817f2d356811d4566
전철호 <cheolho@wisoft.io>
2021-04-07 17:18:25 +0900

    Refactoring Q003

--- src/main/kotlin/Q003/Application.kt
+++ src/main/kotlin/Q003/Application.kt
@@ -7,11 +7,15 @@ fun main() {
   solution("qwer") eq "we"
 }
 
-// fun solution(s: String) = s.substring((s.length - 1) / 2, s.length / 2 + 1)
+ fun solution(s: String) = s.substring(
+   startIndex = (s.length - 1) / 2,
+   endIndex = s.length / 2 + 1,
+ )
+
 // fun solution(s: String) = s.slice(((s.length - 1) / 2)..(s.length / 2))
 
-fun solution(s: String): String {
-  val start = (s.length - 1) / 2
-  val end = s.length / 2
-  return s.slice(start..end)
-}
+//fun solution(s: String): String {
+//  val start = (s.length - 1) / 2
+//  val end = s.length / 2
+//  return s.slice(start..end)
+//}
